c3c8c7f8ab0f66414c2ff8bc38199ff009252496
Germán Arevalo <germanarevalojerez@gmail.com>
2019-06-17 11:46:00 -0500

    GET and POST

--- src/main/kotlin/servidor/AppServer.kt
+++ src/main/kotlin/servidor/AppServer.kt
@@ -1,18 +1,57 @@
 package servidor
 
-import io.ktor.server.netty.*
-import io.ktor.routing.*
+import com.google.gson.GsonBuilder
 import io.ktor.application.*
-import io.ktor.http.*
-import io.ktor.response.*
-import io.ktor.server.engine.*
+import io.ktor.features.CallLogging
+import io.ktor.http.ContentType
+import io.ktor.http.DEFAULT_PORT
+import io.ktor.http.HttpStatusCode
+import io.ktor.request.receiveText
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.post
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import io.netty.handler.codec.DefaultHeaders
+import java.lang.NumberFormatException
+import javax.sound.sampled.Port
 
-fun main() {
-    embeddedServer(Netty, 8080) {
-        routing {
-            get("/") {
-                call.respondText("Hello, world!", ContentType.Text.Html)
+fun Application.main() {
+    install(io.ktor.features.DefaultHeaders)
+    install(CallLogging)
+
+    routing {
+        get("/me") {
+            val name = call.request.queryParameters["name"]
+            val age = call.request.queryParameters["age"]
+
+            val gson = GsonBuilder().setPrettyPrinting().create()
+
+            if (name != null && age != null) {
+                val edad = try {
+                    age.toInt()
+                }catch (e:NumberFormatException){
+                    0
+                }
+                val me = Persona(name, edad)
+
+                val jsonRes = gson.toJson(me)
+                call.respondText(jsonRes, ContentType.Application.Json, HttpStatusCode.OK)
+            } else {
+                val error = Error(HttpStatusCode.BadRequest.value, "Datos invalidos")
+                call.respondText(gson.toJson(error), ContentType.Application.Json, HttpStatusCode.BadRequest)
             }
         }
-    }.start(wait = true)
+
+        post("/postme") {
+            val messagepost = call.receiveText()
+            println(messagepost)
+            call.respondText("ok", ContentType.Text.Html, HttpStatusCode.OK)
+        }
+    }
+}
+
+fun main() {
+    embeddedServer(Netty, 8080, watchPaths = listOf("AppServerKt"), module = Application::main).start()
 }
